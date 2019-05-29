3e3e4cda631d08a81cd47211e89ccb13e7aafcee
malagutti <upmalagutti@gmail.com>
2019-05-29 23:09:52 +0200

    Mixing components (Librabry and Book)

--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backCountry: 10,
-  goal: 100
+const Library = () => {
+  return (
+    <div>
+      <h1>Welcome to the Library!</h1>
+      <Book title="The sun Also Rises" author="Ernest Hemingway" pages={260}/>
+      <Book title="White Teeth" author="Zadie Smith" pages={480}/>
+      <Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+    </div>
+  )
 };
 
-const getPercentage = (decimal) => (decimal * 100) + '%';
-
-const calculateGoalProgress = (total, goal) => getPercentage(total / goal);
-
-const SkiDataCounter = ({total, powder, backCountry, goal}) => {
+const Book = ({title, author, pages}) => {
   return (
     <section>
-      <div>
-        <p>Total days: {total}</p>
-      </div>
-      <div>
-        <p>Powder days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry days: {backCountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calculateGoalProgress(total, goal)}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>By: {author}</p>
+      <p>Pages: {pages} pages</p>
     </section>
-  );
-}
+  )
+};
 
 ReactDOM.render(
-  <SkiDataCounter 
-    total={skiData.total}
-    powder={skiData.powder}
-    backCountry={skiData.backCountry}
-    goal={skiData.goal} />,
+  <Library />,
   document.getElementById('root')
 );
