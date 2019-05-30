2f7ea0516b5c06ff53fd912aa81e117359b12f1f
malagutti <upmalagutti@gmail.com>
2019-05-30 11:16:02 +0200

    Fetching and displaying dynamic data using the state of the component

--- src/index.js
+++ src/index.js
@@ -34,11 +34,16 @@ class Library extends React.Component {
   state = { 
     open: false,
     freeBookmark: true,
-    hiring: true
+    hiring: true,
+    data: [],
+    loading: false
   };
   
   componentDidMount() {
-    console.log("The component is now mounted.");
+    this.setState({ loading: true });
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+      .then(data => data.json())
+      .then(data => this.setState({ data, loading: false }));
   }
 
   componentDidUpdate() {
@@ -55,7 +60,21 @@ class Library extends React.Component {
     const books = this.props.books;
     return (
       <div>
+        
         {this.state.hiring ? <Hiring/> : <NotHiring/>}
+        
+        {this.state.loading ? "loading..." : <div>
+          {this.state.data.map((product, index) => {
+            return (
+              <div key={index}>
+                <h3>Library Product of the Week!</h3>
+                <h4>{product.name}</h4>
+                <img src={product.image} height={100}/>
+              </div>
+            )
+          })}
+        </div>}
+
         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map(
