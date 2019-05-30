018a45514edd281919cf7c2d8333d155efb95fa1
malagutti <upmalagutti@gmail.com>
2019-05-30 11:27:02 +0200

    Added small example of the use of a React Form

--- src/index.js
+++ src/index.js
@@ -1,109 +1,130 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const bookList = [
-  {
-    title: "The sun Also Rises",
-    author: "Ernest Hemingway",
-    pages: 260
-  },
-  {
-    title: "White Teeth",
-    author: "Zadie Smith",
-    pages: 480
-  },
-  {
-    title: "Cat's Cradle",
-    author: "Kurt Vonnegut",
-    pages: 304
-  }
-];
+// const bookList = [
+//   {
+//     title: "The sun Also Rises",
+//     author: "Ernest Hemingway",
+//     pages: 260
+//   },
+//   {
+//     title: "White Teeth",
+//     author: "Zadie Smith",
+//     pages: 480
+//   },
+//   {
+//     title: "Cat's Cradle",
+//     author: "Kurt Vonnegut",
+//     pages: 304
+//   }
+// ];
 
-const Hiring = () => 
-  <div>
-    <p>The library is hiring! Visit our website for more information.</p>
-  </div>
+// const Hiring = () => 
+//   <div>
+//     <p>The library is hiring! Visit our website for more information.</p>
+//   </div>
 
-const NotHiring = () =>
-  <div>
-    <p>The library is not hiring at the moment. Check back later for more info. Thank you.</p>
-  </div>
+// const NotHiring = () =>
+//   <div>
+//     <p>The library is not hiring at the moment. Check back later for more info. Thank you.</p>
+//   </div>
 
-class Library extends React.Component {
+// class Library extends React.Component {
 
-  state = { 
-    open: false,
-    freeBookmark: true,
-    hiring: true,
-    data: [],
-    loading: false
-  };
+//   state = { 
+//     open: false,
+//     freeBookmark: true,
+//     hiring: true,
+//     data: [],
+//     loading: false
+//   };
   
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
-      .then(data => data.json())
-      .then(data => this.setState({ data, loading: false }));
-  }
+//   componentDidMount() {
+//     this.setState({ loading: true });
+//     fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+//       .then(data => data.json())
+//       .then(data => this.setState({ data, loading: false }));
+//   }
 
-  componentDidUpdate() {
-    console.log("The component has been updated.");
-  }
+//   componentDidUpdate() {
+//     console.log("The component has been updated.");
+//   }
 
-  toggleOpenClosed = () => {
-    this.setState(prevState => ({
-      open: !prevState.open
-    }));
+//   toggleOpenClosed = () => {
+//     this.setState(prevState => ({
+//       open: !prevState.open
+//     }));
+//   }
+
+//   render() {
+//     const books = this.props.books;
+//     return (
+//       <div>
+        
+//         {this.state.hiring ? <Hiring/> : <NotHiring/>}
+        
+//         {this.state.loading ? "loading..." : <div>
+//           {this.state.data.map(product => {
+//             return (
+//               <div key={product.id}>
+//                 <h3>Library Product of the Week!</h3>
+//                 <h4>{product.name}</h4>
+//                 <img alt={product.name} src={product.image} height={100}/>
+//               </div>
+//             )
+//           })}
+//         </div>}
+
+//         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+//         <button onClick={this.toggleOpenClosed}>Change</button>
+//         {books.map(
+//           (book, index) =>
+//            <Book 
+//             key={index} 
+//             title={book.title} 
+//             author={book.author} 
+//             pages={book.pages} 
+//             freeBookmark={this.state.freeBookmark} 
+//           />
+//         )}
+//       </div>
+//     )
+//   }
+// }
+
+// const Book = ({title, author, pages, freeBookmark}) => {
+//   return (
+//     <section>
+//       <h2>{title}</h2>
+//       <p>By: {author}</p>
+//       <p>Pages: {pages} pages</p>
+//       <p>Free Bookmark today: {freeBookmark ? 'Yes' : 'No'}</p>
+//     </section>
+//   )
+// };
+
+
+class FavoriteColorForm extends React.Component {
+  state = { value: '' };
+  newColor = event => this.setState({ value: event.target.value })
+  submit = event => {
+    console.log(`The new favorite color is ${this.state.value}`);
+    event.preventDefault();
   }
 
   render() {
-    const books = this.props.books;
     return (
-      <div>
-        
-        {this.state.hiring ? <Hiring/> : <NotHiring/>}
-        
-        {this.state.loading ? "loading..." : <div>
-          {this.state.data.map(product => {
-            return (
-              <div key={product.id}>
-                <h3>Library Product of the Week!</h3>
-                <h4>{product.name}</h4>
-                <img alt={product.name} src={product.image} height={100}/>
-              </div>
-            )
-          })}
-        </div>}
-
-        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-        <button onClick={this.toggleOpenClosed}>Change</button>
-        {books.map(
-          (book, index) =>
-           <Book 
-            key={index} 
-            title={book.title} 
-            author={book.author} 
-            pages={book.pages} 
-            freeBookmark={this.state.freeBookmark} 
-          />
-        )}
-      </div>
+      <form onSubmit={this.submit}>
+        <label> Favorite Color:
+          <input type="color" onChange={this.newColor}></input>
+        </label>
+        <button>Submit</button>
+      </form>
     )
   }
 }
 
-const Book = ({title, author, pages, freeBookmark}) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <p>By: {author}</p>
-      <p>Pages: {pages} pages</p>
-      <p>Free Bookmark today: {freeBookmark ? 'Yes' : 'No'}</p>
-    </section>
-  )
-};
-
 ReactDOM.render(
-  <Library books={bookList} />,
+  <FavoriteColorForm />,
   document.getElementById('root')
 );
